bff8b20c5a30f7ac5bc3201ba4806aeabb54f2bd
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-24 21:19:04 +0700

    tugas 20 Muhammad Rizwan

---
+++ tugas20.js
@@ -0,0 +1,10 @@
+/* Tugas 20 Javascript */
+// Muhammad Rizwan
+
+function tugasDuaPuluh(){
+    var words = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+    console.log(words.match("are"));
+    console.log(words.match("words"));
+}
+
+tugasDuaPuluh();
